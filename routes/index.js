083c00139c3389814fe2665e05abe142eeb226d4
thomaslanenh <thomaslanenh@gmail.com>
2022-10-11 10:51:45 -0400

    PDF

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const { AwesomeQR } = require("awesome-qr");
 const fs = require("fs");
 const PDFDocument = require('pdfkit');
 const stream = require('stream');
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -31,13 +32,40 @@ router.get('/barcode',async (req,res,next)=>{
 
 router.get('/print', async (req,res,next) => {
 
-  const doc = new PDFDocument;
-  //
-  // doc.pipe(fs.createWriteStream('public/docs/ticket.pdf'));
-  //
-  // doc.end();
-  res.send({
-    print: "Your item is printing."
+  const doc = new PDFDocument({bufferPages: true});
+
+  let buffers = [];
+
+  var config = {
+    method: 'get',
+    url: '/barcode?hubid=2525225',
+    headers: { }
+  };
+
+  const barcode = router.get(`/barcode${req.query.hubid}`)
+
+  doc.pipe(fs.createWriteStream('public/docs/ticket.pdf'));
+  doc.addPage();
+
+  doc.on('data', buffers.push.bind(buffers));
+
+  doc.on('end', () => {
+    let pdfData = Buffer.concat(buffers);
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(pdfData),
+      'Content-Type': "application/pdf",
+      'Content-disposition': 'attachment;filename=test.pdf',
+    })
+        .end(pdfData)
   })
+
+  var img = Buffer.from(fs.readFileSync("qrcode.png"), 'base64');
+  doc.text(`Season Pass ${req.query.hubid}`)
+  doc.image(img, 230, 220);
+  doc.end();
+
+  // res.send({
+  //   print: "Your item is printing."
+  // })
 })
 module.exports = router;
