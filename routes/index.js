b353bec968af099f3a20173e4bf91d011e815d57
thomaslanenh <thomaslanenh@gmail.com>
2022-10-11 10:21:50 -0400

    Changed Size of QR

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const { AwesomeQR } = require("awesome-qr");
 const fs = require("fs");
+const PDFDocument = require('pdfkit');
+const stream = require('stream');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,19 +16,26 @@ router.get('/barcode',async (req,res,next)=>{
 
   const buffer = await new AwesomeQR({
     text: req.query.hubid,
-    size: 500,
+    size: 300,
     backgroundImage: background
   }).draw();
 
   fs.writeFileSync("qrcode.png", buffer);
 
-  const img = fs.readFileSync("qrcode.png");
-  res.writeHead(200, {'Content-Type': 'image/gif'});
-  res.end(img, 'binary');
-})
+  var img = Buffer.from(fs.readFileSync("qrcode.png"), 'base64');
+  res.set({'Content-Type': 'image/gif', 'Content-Length': img.length});
+  res.end(img);
 
+    }
+)
 
 router.get('/print', async (req,res,next) => {
+
+  const doc = new PDFDocument;
+  //
+  // doc.pipe(fs.createWriteStream('public/docs/ticket.pdf'));
+  //
+  // doc.end();
   res.send({
     print: "Your item is printing."
   })
